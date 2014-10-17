b057e9e3775a880d431d3a8493db5fa67dd09788
Jan Machacek <jan.machacek@gmail.com>
2014-10-17 17:16:49 +0100

    ML

--- ml/src/main/scala/com/eigengo/lift/ml/Parser.scala
+++ ml/src/main/scala/com/eigengo/lift/ml/Parser.scala
@@ -1,5 +1,7 @@
 package com.eigengo.lift.ml
 
+import java.nio.ByteBuffer
+
 object PebbleAccelerometerParser {
 
   case class AccelerometerData(x: Int, y: Int, z: Int)
@@ -9,10 +11,9 @@ object PebbleAccelerometerParser {
 class PebbleAccelerometerParser {
   import PebbleAccelerometerParser._
 
-  def parse(bytes: Array[Byte]): AccelerometerData = {
+  def parse(bytes: ByteBuffer): AccelerometerData = {
     // FF, FF, FF, FF, 01 is pad->x_val = -1; pad->y_val = -1; pad->z_val = -1;
     // 01, 08, 40, 00, 00 is pad->x_val = +1; pad->y_val = +1; pad->z_val = +1;
-
     // 8C, C0, 39, FA, 00 is pad->x_val = 140; pad->y_val = -200; pad->z_val = 1000;
 
 
@@ -20,8 +21,7 @@ class PebbleAccelerometerParser {
     // y  5B from (1): 3-7 + 6B from (2): 0-5
     // z  2B from (2): 6-7 + 8B from (3): 0-7 + 1B from (3) 0
 
-    val x: Int = bytes(0) + bytes(1) & 3
-    val y = bytes(1)
+    ???
   }
 
 }
