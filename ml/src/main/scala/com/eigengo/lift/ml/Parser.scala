d8a8865d39e21441e7adde91052f0f0bce58c743
Jan Machacek <jan.machacek@gmail.com>
2014-10-20 09:57:48 +0100

    Exported data

--- ml/src/main/scala/com/eigengo/lift/ml/Parser.scala
+++ ml/src/main/scala/com/eigengo/lift/ml/Parser.scala
@@ -4,7 +4,6 @@ import scodec.Codec
 import scodec.bits.BitVector
 
 import scala.collection.mutable.ListBuffer
-import scalaz.{\/-, \/}
 
 trait PebbleAccelerometerParser {
   import scodec.codecs._
@@ -19,24 +18,6 @@ trait PebbleAccelerometerParser {
   private implicit val packedGfsHeader: Codec[CSU] = new FixedSizeReversedCodec(40, {
     ("samplesPerSecond" | int8) :: ("count" | int16) :: constant(BitVector(0xfe, 0xf7))
   }).as[CSU]
-
-  def parsePackedAccelerometerData(bytes: BitVector): \/[String, (BitVector, AccelerometerValue)] = {
-    Codec.decode[ZYX](bytes).map {
-      case (bv, (z, y, x)) => (bv, AccelerometerValue(x, y, z))
-    }
-    // FF, FF, FF, FF, 01 is pad->x_val = -1; pad->y_val = -1; pad->z_val = -1;
-    // 01, 08, 40, 00, 00 is pad->x_val = +1; pad->y_val = +1; pad->z_val = +1;
-    // 8C, C0, 39, FA, 00 is pad->x_val = 140; pad->y_val = -200; pad->z_val = 1000;
-
-
-    // x  8B from (0): 0-7 + 3B from (1): 0-2
-    // y  5B from (1): 3-7 + 6B from (2): 0-5
-    // z  2B from (2): 6-7 + 8B from (3): 0-7 + 1B from (3) 0
-  }
-
-  def parseGfsHeader(bytes: BitVector) = {
-    Codec.decode[CSU](bytes)
-  }
 
   def parse(bits: BitVector): List[AccelerometerData] = {
     implicit val _ = scalaz.Monoid.instance[String](_ + _, "")
--- ml/src/test/scala/com/eigengo/lift/ml/PebbleAccelerometerParserTest.scala
+++ ml/src/test/scala/com/eigengo/lift/ml/PebbleAccelerometerParserTest.scala
@@ -1,10 +1,6 @@
 package com.eigengo.lift.ml
 
-import java.io.{FileOutputStream, InputStreamReader, File, FileInputStream}
-
 import scodec.bits.BitVector
-
-import scala.io.Source
 
 object PebbleAccelerometerParserTest extends App {
   // f7fe 0300 64 -> count = 3, 100 Hz
@@ -14,7 +10,7 @@ object PebbleAccelerometerParserTest extends App {
   // f7fe 0100 64 -> count = 1, 100 Hz
   // 8cc0 39fa 00 -> 140, -200, 1000
 
-  import PebbleAccelerometerParser._
+  import com.eigengo.lift.ml.PebbleAccelerometerParser._
 
   val three = BitVector(
     0xf7, 0xfe, 0x03, 0x00, 0x64,
@@ -24,12 +20,6 @@ object PebbleAccelerometerParserTest extends App {
     0xf7, 0xfe, 0x01, 0x00, 0x64,
     0x8c, 0xc0, 0x39, 0xfA, 0x00)
 
-  println(parseGfsHeader(three))
-
   println(parse(three))
-
-  println(parsePackedAccelerometerData(BitVector(0xFF, 0xFF, 0xFF, 0xFF, 0x01)))
-  println(parsePackedAccelerometerData(BitVector(0x01, 0x08, 0x40, 0x00, 0x00)))
-  println(parsePackedAccelerometerData(BitVector(0x8C, 0xC0, 0x39, 0xFA, 0x00)))
 
 }
