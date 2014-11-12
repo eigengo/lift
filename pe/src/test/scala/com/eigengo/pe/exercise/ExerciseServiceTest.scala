70f4bcd633bd5097da971f9533183017e8233c73
Jan Machacek <jan.machacek@gmail.com>
2014-11-12 08:01:56 +0000

    Tests

--- pe/src/test/scala/com/eigengo/pe/exercise/ExerciseServiceTest.scala
+++ pe/src/test/scala/com/eigengo/pe/exercise/ExerciseServiceTest.scala
@@ -1,18 +1,18 @@
 package com.eigengo.pe.exercise
 
 import akka.actor.{ActorRef, ActorRefFactory}
-import akka.testkit.{TestActor, TestActorRef}
+import akka.testkit.TestKitBase
 import com.eigengo.pe.{LiftMarshallers, LiftTestMarshallers}
 import org.scalatest.{FlatSpec, Matchers}
 import scodec.bits.BitVector
 import spray.testkit.ScalatestRouteTest
 
 class ExerciseServiceTest
-  extends FlatSpec with ScalatestRouteTest with Matchers
+  extends FlatSpec with ScalatestRouteTest with TestKitBase with Matchers
   with ExerciseService with LiftMarshallers with LiftTestMarshallers {
 
   def actorRefFactory: ActorRefFactory = system
-  override val exercise: ActorRef = TestActorRef[TestActor]
+  override val exercise: ActorRef = testActor
 
   def getResourceBitVector(resourceName: String): BitVector = {
     val is = getClass.getResourceAsStream(resourceName)
