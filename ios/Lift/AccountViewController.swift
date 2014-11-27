3112d7048ed1103470458bd91468ea704ef3106f
Jan Machacek <jan.machacek@gmail.com>
2014-11-27 17:54:19 +0000

    Swiftz

---
+++ ios/Lift/AccountViewController.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+class AccountViewController : UIViewController {
+    @IBOutlet var username: UITextField!
+    @IBOutlet var password: UITextField!
+    
+    private func showError(error: NSError) {
+        
+    }
+    
+    private func showAccount(user: User) {
+        performSegueWithIdentifier("account_account", sender: nil)        
+    }
+    
+    @IBAction
+    func login(sender: UIButton) {
+        LiftServer.sharedInstance.login(username.text, password: password.text).cata(showError, showAccount)
+    }
+    
+    @IBAction
+    func register(sender: UIButton) {
+        LiftServer.sharedInstance.register(username.text, password: password.text).cata(showError, showAccount)
+    }
+}
