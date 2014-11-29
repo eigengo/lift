01f16f2a3e9dd09fb9c97fe5b1b40be9cc883da6
Jan Machacek <jan.machacek@gmail.com>
2014-11-29 16:47:31 +0000

    Latest updates

--- ios/Lift/LiftViewController.swift
+++ ios/Lift/LiftViewController.swift
@@ -2,7 +2,12 @@ import Foundation
 
 extension UIViewController {
     
-    func setBackgroundImage(imageName: String) {
+    func setBackgroundImage(imageNames: String...) {
+        if (imageNames.count == 0) {
+            return
+        }
+        
+        let imageName = imageNames[random() % imageNames.count]
         let backgroundImage = UIImageView(image: UIImage(named: imageName))
         backgroundImage.alpha = 0.3
         backgroundImage.contentMode = UIViewContentMode.ScaleAspectFill
