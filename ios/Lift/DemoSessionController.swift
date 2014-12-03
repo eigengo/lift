10a682f6b620665e690b5e0600f73ca5ac26be72
Jan Machacek <jan.machacek@gmail.com>
2014-12-03 10:56:08 +0000

    Demo session

--- ios/Lift/DemoSessionController.swift
+++ ios/Lift/DemoSessionController.swift
@@ -1,6 +1,38 @@
 import Foundation
 
-class DemoSessionController : UIViewController, UITabBarDelegate, MuscleGroupsSettable {
+class DemoSessionTableModel : NSObject, UITableViewDataSource {
+    let dataFiles = ["foo", "bar", "baz"]
+    
+    // #pragma mark - UITableViewDataSource
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataFiles.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell  {
+        let data = dataFiles[indexPath.row]
+        let cell = tableView.dequeueReusableCellWithIdentifier("default") as UITableViewCell
+        
+        cell.textLabel!.text = data
+        cell.detailTextLabel!.text = "xyz B"
+        
+        return cell
+    }
+
+}
+
+class DemoSessionController : UIViewController, UITabBarDelegate, UITableViewDelegate, MuscleGroupsSettable {
+    @IBOutlet
+    var tableView: UITableView!
+    let tableModel = DemoSessionTableModel()
+    
+    override func viewDidLoad() {
+        tableView.delegate = self
+        tableView.dataSource = tableModel
+    }
     
     func setMuscleGroupKeys(muscleGroupKeys: [String]) {
         NSLog("Starting with %@", muscleGroupKeys)
@@ -10,5 +42,9 @@ class DemoSessionController : UIViewController, UITabBarDelegate, MuscleGroupsSe
         // we only have one item, so back we go
         performSegueWithIdentifier("end", sender: nil)
     }
-    
+ 
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        NSLog("Send ADs")
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
 }
