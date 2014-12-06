4ceb5e2fb455fe04f3b954c2acff61c7fd778241
Jan Machacek <jan.machacek@gmail.com>
2014-12-06 16:06:54 +0000

    Chartery

--- ios/Lift/SessionsController.swift
+++ ios/Lift/SessionsController.swift
@@ -1,5 +1,51 @@
 import Foundation
 
+class SessionTableViewCell : UITableViewCell, JBBarChartViewDataSource, JBBarChartViewDelegate {
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var detailLabel: UILabel!
+    @IBOutlet var chartView: UIView!
+    private var intensityChart: JBBarChartView?
+    private var sessionSummary: Exercise.SessionSummary?
+
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func awakeFromNib() {
+        self.intensityChart = JBBarChartView()
+        self.intensityChart!.dataSource = self
+        self.intensityChart!.delegate = self
+        self.intensityChart!.frame = self.chartView.frame
+        addSubview(self.intensityChart!)
+    }
+    
+    func setSessionSummary(sessionSummary: Exercise.SessionSummary) {
+        self.sessionSummary = sessionSummary
+        let props = sessionSummary.sessionProps
+        
+        self.titleLabel.textColor = props.intendedIntensity.textColor()
+        let dateText = NSDateFormatter.localizedStringFromDate(props.startDate, dateStyle: NSDateFormatterStyle.MediumStyle, timeStyle: NSDateFormatterStyle.MediumStyle)
+        // let ints = " ".join(sessionSummary.setIntensities.map { x in return String(format:"%f", x) })
+        self.detailLabel.text = "On \(dateText)"
+        self.intensityChart!.reloadData()
+    }
+    
+    func numberOfBarsInBarChartView(barChartView: JBBarChartView!) -> UInt {
+        if self.sessionSummary != nil {
+            return 5 //UInt(self.sessionSummary!.setIntensities.count)
+        }
+        return 0
+    }
+
+    func barChartView(barChartView: JBBarChartView, colorForBarViewAtIndex index: UInt) -> UIColor {
+        return self.sessionSummary!.setIntensities[0].textColor()
+    }
+
+    func barChartView(barChartView: JBBarChartView!, heightForBarViewAtIndex index: UInt) -> CGFloat {
+        return 100 //CGFloat(self.sessionSummary!.setIntensities[Int(index)]) * 20
+    }
+}
+
 class SessionsController : UITableViewController, UITableViewDataSource {
     private var sessionSummaries: [Exercise.SessionSummary] = []
     
@@ -8,6 +54,10 @@ class SessionsController : UITableViewController, UITableViewDataSource {
             self.sessionSummaries = $0.fold([], identity)
             self.tableView.reloadData()
         }
+    }
+    
+    override func viewDidLoad() {
+        self.tableView.registerNib(UINib(nibName: "SessionTableCell", bundle: nil), forCellReuseIdentifier: "default")
     }
 
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
@@ -40,15 +90,8 @@ class SessionsController : UITableViewController, UITableViewDataSource {
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell  {
         let sessionSummary = sessionSummaries[indexPath.row]
-        let props = sessionSummary.sessionProps
-        let cell = tableView.dequeueReusableCellWithIdentifier("default") as UITableViewCell
-        cell.textLabel!.textColor = props.intendedIntensity.textColor()        
-        let dateText = NSDateFormatter.localizedStringFromDate(props.startDate, dateStyle: NSDateFormatterStyle.MediumStyle, timeStyle: NSDateFormatterStyle.MediumStyle)
-        cell.textLabel!.text = ", ".join(props.muscleGroupKeys)
-        
-        let ints = " ".join(sessionSummary.setIntensities.map { x in return String(format:"%f", x) })
-        cell.detailTextLabel!.text = "On \(dateText) \(ints)"
-        
+        let cell = tableView.dequeueReusableCellWithIdentifier("default") as SessionTableViewCell
+        cell.setSessionSummary(sessionSummary)
         return cell
     }
     
