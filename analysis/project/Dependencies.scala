9147f81c7798dcd0370a31b4b2b06539f2ed18f7
Jan Machacek <jan.machacek@gmail.com>
2014-12-18 15:59:39 +0000

    Intransitive

--- analysis/project/Dependencies.scala
+++ analysis/project/Dependencies.scala
@@ -12,9 +12,9 @@ object Dependencies {
     val version = "1.1.0"
 
     val core = "org.apache.spark" %% "spark-core" % version % "provided"
-    val streaming = "org.apache.spark" %% "spark-streaming" % version % "provided"
-    val streaming_kafka = "org.apache.spark" %% "spark-streaming-kafka" % version % "provided"
-    val mllib = "org.apache.spark" %% "spark-mllib" % version
+    val streaming = "org.apache.spark" %% "spark-streaming" % version % intransitive()
+    val streaming_kafka = "org.apache.spark" %% "spark-streaming-kafka" % version % intransitive()
+    val mllib = "org.apache.spark" %% "spark-mllib" % version % intransitive()
   }
 
   object scalaz {
