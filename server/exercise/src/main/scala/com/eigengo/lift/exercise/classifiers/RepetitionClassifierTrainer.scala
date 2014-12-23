76a3c917dbedeb91f724df9fb2f990b2d3e79c72
Jan Machacek <jan.machacek@gmail.com>
2014-12-23 09:58:02 +0000

    Trainer

--- server/exercise/src/main/scala/com/eigengo/lift/exercise/classifiers/RepetitionClassifierTrainer.scala
+++ server/exercise/src/main/scala/com/eigengo/lift/exercise/classifiers/RepetitionClassifierTrainer.scala
@@ -9,13 +9,16 @@ trait RepetitionClassifierTrainer {
   import nak.liblinear.{SolverType, LiblinearConfig}
 
   def train(principalAxis: DenseVector[Int], muscleGroupKey: MuscleGroupKey, exerciseName: ExerciseName): Unit = {
-    val npa = normalize(principalAxis.map(_.toDouble))
-    val feature = 1
-    val example = Example(feature, npa.mapPairs{ case (i, x) ⇒ FeatureObservation(i, x) }.valuesIterator.toList)
     val config = LiblinearConfig(cost = 0.1)
-    val model = trainModel(config, List(example), npa.length)
+    val featurizer = new Featurizer[DenseVector[Int], String] {
+      override def apply(v: DenseVector[Int]): Seq[FeatureObservation[String]] =
+        v.map(x ⇒ FeatureObservation(exerciseName)).toScalaVector()
+    }
+    val example = Example(exerciseName, principalAxis)
+    val classifier = trainClassifier(config, featurizer, List(example))
 
-    println(model)
+    val res = classifier.predict(principalAxis)
+    println(res)
   }
 
 }
