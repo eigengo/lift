6ec6409ecfef97b406c5003cad9a8d03c9a9dacf
Jan Machacek <jan.machacek@gmail.com>
2014-12-28 23:42:24 +0000

    Home view

---
+++ ios/Lift/UIViewController+ParallaxBlur.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+extension UIViewController {
+
+    func setHeaderImage(headerImage: UIImage) {
+        
+    }
+    
+    func addHeaderOverlayView(overlayView: UIView) {
+        
+    }
+    
+    func setHeaderHeight(height: CGFloat) {
+        
+    }
+        
+}
