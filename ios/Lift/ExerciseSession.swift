b335b0097963ea3c2bd3913f8b2357666a82c5e1
Jan Machacek <jan.machacek@gmail.com>
2015-01-18 17:47:25 +0000

    Sessions with caching

--- ios/Lift/ExerciseSession.swift
+++ ios/Lift/ExerciseSession.swift
@@ -1,10 +1,17 @@
 import Foundation
 
+/**
+ * Models an exercise session. 
+ */
 class ExerciseSession : NSObject {
     var props: Exercise.SessionProps
     var id: NSUUID
     var isOffline: Bool
     
+    /**
+     * Constructs a session with the given id, props and indicator whether the session is offline
+     * right from the start.
+     */
     init(id: NSUUID, props: Exercise.SessionProps, isOffline: Bool) {
         self.id = id
         self.props = props
@@ -12,6 +19,9 @@ class ExerciseSession : NSObject {
         super.init()
     }
     
+    /**
+     * Submits multi-packet for the current session
+     */
     func submitData(mp: MultiPacket) {
         if isOffline {
             NSLog("Offline: Saved MP.")
@@ -21,25 +31,43 @@ class ExerciseSession : NSObject {
         }
     }
     
+    /**
+     * Ends the current session. After ending, this instance is not re-usable
+     */
     func end() {
         LiftServer.sharedInstance.exerciseSessionEnd(CurrentLiftUser.userId!, sessionId: id) { _ in }
     }
     
+    /**
+     * Obtain classification examples for the given session
+     */
     func getClassificationExamples(f: Result<[Exercise.Exercise]> -> Void) -> Void {
         return LiftServer.sharedInstance.exerciseSessionGetClassificationExamples(CurrentLiftUser.userId!, sessionId: id, f)
     }
     
+    /** 
+     * Start explicit classification of the given exercise
+     */
     func startExplicitClassification(exercise: Exercise.Exercise) -> Void {
         LiftServer.sharedInstance.exerciseSessionStartExplicitClassification(CurrentLiftUser.userId!, sessionId: id, exercise: exercise, f: const(()))
     }
     
+    /** 
+     * End explicit classification
+     */
     func endExplicitClassification() {
         LiftServer.sharedInstance.exerciseSessionEndExplicitClassification(CurrentLiftUser.userId!, sessionId: id, f: const(()))
     }
 }
 
+/**
+ * Allows a session to be set
+ */
 @objc protocol ExerciseSessionSettable {
     
+    /**
+     * Typically invoked after a sesion is constructed and ready to be started
+     */
     func setExerciseSession(session: ExerciseSession)
     
 }
