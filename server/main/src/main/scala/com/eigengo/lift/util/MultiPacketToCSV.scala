60dbad8974258c5ca036b10190707b51b8dad586
Carl Pulley <carlp@cakesolutions.net>
2015-01-23 22:50:04 +0000

    Simple Scala multi-packet to CSV translator

---
+++ server/main/src/main/scala/com/eigengo/lift/util/MultiPacketToCSV.scala
@@ -0,0 +1,38 @@
+package com.eigengo.lift.util
+
+import com.eigengo.lift.exercise._
+import java.io._
+import scodec.bits.BitVector
+
+/**
+ * Simple utility application for reading in raw binary accelerometer data, decoding it and then dumping it to a CSV file.
+ */
+object MultiPacketToCSV extends App {
+
+  if (args.length != 2) {
+    println("Usage: AccelerometerDataToCSV <input raw accelerometer binary filename> <output CSV filename>")
+    sys.exit(1)
+  }
+
+  val inFileName = args(0)
+  val outFileName = args(1)
+
+  val decoderData = BitVector.fromMmap(new FileInputStream(new File(inFileName)).getChannel)
+
+  val fd = new FileWriter(outFileName, true)
+  try {
+    fd.write("\"location\",\"samplingRate\",\"x\",\"y\",\"z\"\n")
+    for (block <- MultiPacketDecoder.decode(decoderData.toByteBuffer)) {
+      for (pkt <- block.packets) {
+        for (data <- RootSensorDataDecoder(AccelerometerDataDecoder).decodeAll(pkt.payload)) {
+          val csv = data.asInstanceOf[List[AccelerometerData]].flatMap { d => d.values.map(v => s"${pkt.sourceLocation},${d.samplingRate},${v.x},${v.y},${v.z}")}.mkString("", "\n", "\n")
+
+          fd.write(csv)
+        }
+      }
+    }
+  } finally {
+    fd.close()
+  }
+
+}
