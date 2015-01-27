ec0d3fba450fa96c3765d06468538d96ed6720b6
Carl Pulley <carlp@cakesolutions.net>
2015-01-27 11:33:13 +0000

    SamplingWindow testing complete

--- server/exercise/src/main/scala/com/eigengo/lift/exercise/classifiers/workflows/SamplingWindow.scala
+++ server/exercise/src/main/scala/com/eigengo/lift/exercise/classifiers/workflows/SamplingWindow.scala
@@ -1,6 +1,6 @@
 package com.eigengo.lift.exercise.classifiers.workflows
 
-import akka.stream.stage.{TerminationDirective, Directive, Context, PushStage}
+import akka.stream.stage.{TerminationDirective, Directive, Context, PushPullStage}
 import scala.collection.mutable
 
 /**
@@ -10,26 +10,32 @@ import scala.collection.mutable
  * @param size   size of the internal buffer and so the sampling window size
  * @param action when buffer is full, action to be performed for each sample that we subsequently receive
  */
-class SamplingWindow[A] private (size: Int, action: List[A] => Unit) extends PushStage[A, A] {
+class SamplingWindow[A] private (size: Int, action: List[A] => Unit) extends PushPullStage[A, A] {
   require(size > 0)
 
   private val buffer = mutable.Queue[A]()
 
   override def onPush(elem: A, ctx: Context[A]): Directive = {
-    if (!ctx.isFinishing) {
-      if (buffer.length == size) {
-        // Buffer is full, so enable action callback
-        val emit = buffer.dequeue()
-        buffer.enqueue(elem)
-        action(buffer.toList)
-        // Allow old events to propagate downstream
-        ctx.push(emit)
-      } else {
-        // Buffer is not yet full, so keep consuming from our upstream
-        buffer.enqueue(elem)
-        ctx.pull()
-      }
+    if (buffer.length == size) {
+      // Buffer is full, so perform action callback
+      val emit = buffer.dequeue()
+      buffer.enqueue(elem)
+      action(buffer.toList)
+      // Allow old events to propagate downstream
+      ctx.push(emit)
     } else {
+      // Buffer is not yet full, so keep consuming from our upstream
+      buffer.enqueue(elem)
+      if (buffer.length == size) {
+        // Buffer is full, so perform action callback
+        action(buffer.toList)
+      }
+      ctx.pull()
+    }
+  }
+
+  override def onPull(ctx: Context[A]): Directive = {
+    if (ctx.isFinishing) {
       // Streaming stage is shutting down, so we ensure that all buffer elements are flushed prior to finishing
       if (buffer.isEmpty) {
         // Buffer is empty, so we simply finish
@@ -41,6 +47,8 @@ class SamplingWindow[A] private (size: Int, action: List[A] => Unit) extends Pus
         // Multiple elements are in the buffer, so reduce its size and eventually we will finish
         ctx.push(buffer.dequeue())
       }
+    } else {
+      ctx.pull()
     }
   }
 
--- server/exercise/src/test/scala/com/eigengo/lift/exercise/classifiers/workflows/SamplingWindowTest.scala
+++ server/exercise/src/test/scala/com/eigengo/lift/exercise/classifiers/workflows/SamplingWindowTest.scala
@@ -10,117 +10,153 @@ class SamplingWindowTest extends AkkaSpec {
   import FlowGraphImplicits._
   import StreamTestKit._
 
-  val settings = MaterializerSettings(system).withInputBuffer(initialSize = 2, maxSize = 16)
+  val settings = MaterializerSettings(system).withInputBuffer(initialSize = 1, maxSize = 1)
 
   implicit val materializer = FlowMaterializer(settings)
 
+  val senderProbe = TestProbe()
+
   def sample(in: Source[String], out: Sink[String])(probe: TestProbe) = FlowGraph { implicit builder =>
     in ~> Flow[String].transform(() => SamplingWindow[String](5) { sample =>
-      probe.ref ! sample
+      probe.ref.tell(sample, senderProbe.ref)
     }) ~> out
   }
 
   "SamplingWindow" must {
     "SamplingWindow should receive elements, but not emit them whilst its internal buffer is not full" in {
       val msgs = List("one", "two", "three")
-      val in = Source(msgs)
+      // Simulate source that outputs messages and then blocks
+      val in = PublisherProbe[String]()
       val out = SubscriberProbe[String]()
       val probe = TestProbe()
 
-      val workflow = sample(in, Sink(out))(probe)
+      val workflow = sample(Source(in), Sink(out))(probe)
       workflow.run()
+      val pub = in.expectSubscription()
       val sub = out.expectSubscription()
       sub.request(msgs.length)
+      for (msg <- msgs) {
+        pub.sendNext(msg)
+      }
 
       probe.expectNoMsg()
       out.expectNoMsg()
+      senderProbe.expectNoMsg()
     }
 
     "a saturated SamplingWindow should emit elements in the order they are received" in {
       val msgs = List("one", "two", "three", "four", "five", "six", "seven")
-      val in = Source(msgs)
+      // Simulate source that outputs messages and then blocks
+      val in = PublisherProbe[String]()
       val out = SubscriberProbe[String]()
       val probe = TestProbe()
 
-      val workflow = sample(in, Sink(out))(probe)
+      val workflow = sample(Source(in), Sink(out))(probe)
       workflow.run()
+      val pub = in.expectSubscription()
       val sub = out.expectSubscription()
       sub.request(msgs.length)
+      for (msg <- msgs) {
+        pub.sendNext(msg)
+      }
 
-      probe.receiveN(2) === msgs.slice(0, 2)
+      probe.receiveN(3) should be(List(msgs.slice(0, 5), msgs.slice(1, 6), msgs.slice(2, 7)))
       out.expectNext(msgs(0), msgs(1))
+      out.expectNoMsg() // since buffer is saturated and no more messages are arriving
+      senderProbe.expectNoMsg()
     }
 
     "closing a partially full SamplingWindow should flush buffered elements" in {
       val msgs = List("one", "two", "three")
-      val in = Source(msgs)
+      // Simulate source that outputs messages and then completes
+      val in = PublisherProbe[String]()
       val out = SubscriberProbe[String]()
       val probe = TestProbe()
 
-      val workflow = sample(in, Sink(out))(probe)
+      val workflow = sample(Source(in), Sink(out))(probe)
       workflow.run()
+      val pub = in.expectSubscription()
       val sub = out.expectSubscription()
       sub.request(msgs.length + 1) // + OnComplete
+      for (msg <- msgs) {
+        pub.sendNext(msg)
+      }
+      pub.sendComplete()
 
       probe.expectNoMsg()
       out.expectNext(msgs(0), msgs(1), msgs.drop(2): _*)
       out.expectComplete()
+      senderProbe.expectNoMsg()
     }
 
     "closing a saturated SamplingWindow should flush buffered elements" in {
       val msgs = List("one", "two", "three", "four", "five", "six", "seven")
-      val in = Source(msgs)
+      // Simulate source that outputs messages and then completes
+      val in = PublisherProbe[String]()
       val out = SubscriberProbe[String]()
       val probe = TestProbe()
 
-      val workflow = sample(in, Sink(out))(probe)
+      val workflow = sample(Source(in), Sink(out))(probe)
       workflow.run()
+      val pub = in.expectSubscription()
       val sub = out.expectSubscription()
       sub.request(msgs.length + 1) // + OnComplete
+      for (msg <- msgs) {
+        pub.sendNext(msg)
+      }
+      pub.sendComplete()
 
-      probe.receiveN(2) === msgs.slice(0, 1)
+      probe.receiveN(3) should be(List(msgs.slice(0, 5), msgs.slice(1, 6), msgs.slice(2, 7)))
       out.expectNext(msgs(0), msgs(1), msgs.drop(2): _*)
       out.expectComplete()
+      senderProbe.expectNoMsg()
     }
 
-    "errors on a partially full SamplingWindow should flush buffered elements" in {
+    "exceptions (i.e. catastrophic stream errors) on a partially full SamplingWindow materialise 'immediately'" in {
       val exn = new RuntimeException("fake error")
       val msgs = List("one", "two", "three")
-      val in = Source[String](() => new Iterable[String] {
-        override def iterator: Iterator[String] =
-          (1 until msgs.length).iterator.map(i => if (i == msgs.length) throw exn else msgs(i))
-      }.iterator)
+      // Simulate source that outputs messages and then errors
+      val in = PublisherProbe[String]()
       val out = SubscriberProbe[String]()
       val probe = TestProbe()
 
-      val workflow = sample(in, Sink(out))(probe)
+      val workflow = sample(Source(in), Sink(out))(probe)
       workflow.run()
+      val pub = in.expectSubscription()
       val sub = out.expectSubscription()
-      sub.request(msgs.length + 1) // + OnError
+      sub.request(msgs.length)
+      for (msg <- msgs) {
+        pub.sendNext(msg)
+      }
+      pub.sendError(exn)
 
       probe.expectNoMsg()
-      out.expectNext(msgs(0), msgs(1), msgs.drop(2): _*)
       out.expectError(exn)
+      senderProbe.expectNoMsg()
     }
 
-    "errors on a saturated SamplingWindow should flush buffered elements" in {
+    "exceptions (i.e. catastrophic stream errors) on a saturated SamplingWindow materialise 'immediately'" in {
       val exn = new RuntimeException("fake error")
       val msgs = List("one", "two", "three", "four", "five", "six")
-      val in = Source[String](() => new Iterable[String] {
-        override def iterator: Iterator[String] =
-          (1 until (msgs.length + 1)).iterator.map(i => if (i == msgs.length) throw exn else if (i > msgs.length) "seven" else msgs(i))
-      }.iterator)
+      // Simulate source that outputs messages and then errors
+      val in = PublisherProbe[String]()
       val out = SubscriberProbe[String]()
       val probe = TestProbe()
 
-      val workflow = sample(in, Sink(out))(probe)
+      val workflow = sample(Source(in), Sink(out))(probe)
       workflow.run()
+      val pub = in.expectSubscription()
       val sub = out.expectSubscription()
-      sub.request(msgs.length + 1) // + OnError
+      sub.request(msgs.length)
+      for (msg <- msgs) {
+        pub.sendNext(msg)
+      }
+      pub.sendError(exn)
 
-      probe.receiveN(1) === msgs.head
-      out.expectNext(msgs(0), msgs(1), msgs.drop(2): _*)
+      probe.receiveN(2) should be(List(msgs.slice(0, 5), msgs.slice(1, 6)))
+      out.expectNext(msgs(0))
       out.expectError(exn)
+      senderProbe.expectNoMsg()
     }
   }
 
