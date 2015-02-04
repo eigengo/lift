1baa833b8e8230b10f129d655ae5892a18e76f51
martinzapletal <zapletal-martin@email.cz>
2015-02-04 22:38:17 +0100

    Driver hostname added

--- server/provisioning/docker/spark-worker/files/run_spark_worker.sh
+++ server/provisioning/docker/spark-worker/files/run_spark_worker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 . /opt/spark-1.1.0/conf/spark-env.sh
-${SPARK_HOME}/bin/spark-class org.apache.spark.deploy.worker.Worker spark://$SPARKMASTER_PORT_7077_TCP_ADDR:$SPARKMASTER_PORT_7077_TCP_PORT
+${SPARK_HOME}/bin/spark-class org.apache.spark.deploy.worker.Worker spark://$SPARKMASTER_PORT_7077_TCP_ADDR:$SPARKMASTER_PORT_7077_TCP_PORT -i $SPARK_LOCAL_IP
