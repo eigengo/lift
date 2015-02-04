0be1d3b2b58d39677455250bfee75fac302a7eac
Jan Machacek <jan.machacek@gmail.com>
2015-02-04 08:34:56 +0000

    Running test detection

--- ios/Lift/LiftUserDefaults.swift
+++ ios/Lift/LiftUserDefaults.swift
@@ -9,10 +9,12 @@ struct LiftUserDefaults {
      * Writes the current value of the logged-in user
      */
     static func setCurrentUserId(userId: NSUUID?) {
-        if userId == nil {
-            NSUserDefaults.standardUserDefaults().setNilValueForKey("userId")
-        } else {
-            NSUserDefaults.standardUserDefaults().setValue(userId!.UUIDString, forKey: "userId")
+        if !isRunningTests {
+            if userId == nil {
+                NSUserDefaults.standardUserDefaults().setNilValueForKey("userId")
+            } else {
+                NSUserDefaults.standardUserDefaults().setValue(userId!.UUIDString, forKey: "userId")
+            }
         }
     }
     
@@ -20,10 +22,24 @@ struct LiftUserDefaults {
      * Retrieves the last known logged-in user
      */
     static func getCurrentUserId() -> NSUUID? {
-        if let x = NSUserDefaults.standardUserDefaults().stringForKey("userId") {
-            return NSUUID(UUIDString: x)
+        if isRunningTests {
+            return NSUUID(UUIDString: "00000000-0000-0000-0000-000000000000")!
+        } else {
+            if let x = NSUserDefaults.standardUserDefaults().stringForKey("userId") {
+                return NSUUID(UUIDString: x)
+            }
+            return nil
         }
-        return nil;
+    }
+    
+    static var isRunningTests: Bool {
+        get {
+            let environment = NSProcessInfo.processInfo().environment
+            if let injectBundle = environment["XCInjectBundle"] as? String {
+                return injectBundle.pathExtension == "xctest"
+            }
+            return false
+        }
     }
     
     static var liftServerUrl: String {
