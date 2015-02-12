dec7de8b1b7a635f2117bc8eba4e95afab332b9f
Carl Pulley <carlp@cakesolutions.net>
2015-02-12 13:04:14 +0000

    Forgot to make the LiftContainerApp and App!!

--- server/main/src/main/scala/com/eigengo/lift/LiftContainerApp.scala
+++ server/main/src/main/scala/com/eigengo/lift/LiftContainerApp.scala
@@ -6,7 +6,7 @@ import akka.actor.{ActorPath, ActorSystem}
  * Docker containerised version of the (local) application. Single JVM and actor system running exercise, notification
  * and profile microservices. REST and Akka ports are fixed for each container.
  */
-object LiftContainerApp extends LocalAppUtil {
+object LiftContainerApp extends App with LocalAppUtil {
 
   // In production, journal and snapshot stores are cassandra based - so setup is configuration/deployment based and so there is no work to do here!
   actorSystemStartUp(2552, 8080, "production.conf", { (ActorSystem, Boolean, ActorPath) => })
