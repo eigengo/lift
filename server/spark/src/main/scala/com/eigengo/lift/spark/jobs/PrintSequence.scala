455eacced5d8ce4ad44514badbafa765b33dc6d0
martinzapletal <zapletal-martin@email.cz>
2015-02-13 11:50:28 +0000

    Build fix

--- server/spark/src/main/scala/com/eigengo/lift/spark/jobs/PrintSequence.scala
+++ server/spark/src/main/scala/com/eigengo/lift/spark/jobs/PrintSequence.scala
@@ -7,21 +7,20 @@ import java.net._
 
 case class PrintSequence() extends Batch[Int, Unit] {
 
-  override def name: String = "PrintCassandra"
+  override def name: String = "PrintSequence"
 
   override def execute(master: String, config: Config, params: Int): Either[String, Unit] = {
 
     println("SPARK DRIVER HOST:")
     println(InetAddress.getLocalHost.getHostAddress)
 
-    val sc = new SparkContext(new SparkConf()
+    /*val sc = new SparkContext(new SparkConf()
       .setAppName(name)
-
       .setMaster(master))
 
     sc.parallelize(1 to params).map(_ + 1).foreach(println)
 
-    sc.stop()
+    sc.stop()*/
 
     Right()
   }
