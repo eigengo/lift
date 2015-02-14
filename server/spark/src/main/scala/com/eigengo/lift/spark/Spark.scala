5d5c167e7011729256f5009b41b7aa8efa3527ec
Carl Pulley <carlp@cakesolutions.net>
2015-02-14 11:44:41 +0000

    Fixes to deal with Spark project compile warnings

--- server/spark/src/main/scala/com/eigengo/lift/spark/Spark.scala
+++ server/spark/src/main/scala/com/eigengo/lift/spark/Spark.scala
@@ -8,22 +8,21 @@ import org.apache.spark.Logging
 
 object Spark extends App with Logging {
 
-  override def main(args: Array[String]): Unit = {
-    val log4jInitialized = Logger.getRootLogger.getAllAppenders.hasMoreElements
-    if (!log4jInitialized) {
-      logInfo("Setting log level to [WARN] for streaming example." +
-        " To override add a custom log4j.properties to the classpath.")
+  val log4jInitialized = Logger.getRootLogger.getAllAppenders.hasMoreElements
+  if (!log4jInitialized) {
+    logInfo("Setting log level to [WARN] for streaming example." +
+      " To override add a custom log4j.properties to the classpath.")
 
-      Logger.getRootLogger.setLevel(Level.WARN)
-    }
-
-    val system = ActorSystem("SparkJobManager")
-
-    val config = ConfigFactory.load()
-    val master = config.getString("spark.master")
-
-    val manager = system.actorOf(JobManager.props(master, config))
-
-    manager ! BatchJobSubmit("PrintSequence")
+    Logger.getRootLogger.setLevel(Level.WARN)
   }
+
+  val system = ActorSystem("SparkJobManager")
+
+  val config = ConfigFactory.load()
+  val master = config.getString("spark.master")
+
+  val manager = system.actorOf(JobManager.props(master, config))
+
+  manager ! BatchJobSubmit("PrintSequence")
+
 }
